7deeb9b33652497cd582e93ad58bf40336757497
Goran Lazic <github@chupcko.org>
2024-07-29 09:38:44 +0200

    Added templates and new script

---
+++ bin/new_problem.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SIZE=${1:-8}
+
+DIR="problems/$(date '+%Y')/$(date '+%Y-%m')"
+TODAY="$(date '+%Y-%m-%d')"
+FILE="${DIR}/${TODAY}.qs"
+
+mkdir -p "${DIR}"
+
+cp "templates/${SIZE}.qs" "${FILE}"
+$EDITOR "${FILE}"
+
+cat <<EOI
+
+git add ${FILE}
+git commit -m "${TODAY}"
+git push
+
+EOI
---
+++ templates/problems.js
@@ -0,0 +1,6 @@
+const Problems = [
+];
+
+export {
+  Problems
+};
